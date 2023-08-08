301f14b8f6131add83a151efb9ea8dc077921774
mostafaf <m.fartaj68@gmail.com>
2023-08-08 13:52:35 +0330

    first edit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>شروع پر قدرت</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          سلام خوش آمدید
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
