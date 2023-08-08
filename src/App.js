15918b47ded70fb8d809806424b3a6922619c944
mostafaf <m.fartaj68@gmail.com>
2023-08-08 14:56:50 +0330

    install & config tailwind

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>شروع پر قدرت</h1>
-        <p>
-          سلام خوش آمدید
-        </p>
-        
-      </header>
+        <div className='flex flex-row'>
+          <div className='basis-3/4 bg-fuchsia-500 text-white py-8'>
+            <h1 className="text-3xl font-bold underline">شروع پر قدرت</h1>
+              <p>  سلام خوش آمدید</p>            
+            </div>
+          <div className='basis-1/4  bg-fuchsia-900 text-white py-8'>
+          </div>
+        </div>
+
+
     </div>
   );
 }
